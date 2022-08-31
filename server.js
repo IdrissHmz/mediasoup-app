bccdf1f24da21e50e3b7d793e6e8e101aa8dbe83
Idriss <idrissham2000@gmail.com>
2022-08-31 16:09:33 +0100

    installation done, everything works

--- server.js
+++ server.js
@@ -1,5 +1,55 @@
-const express = require('express')
+#!/usr/bin/env node
 
-const app = express();
+// process.title = 'mediasoup-demo-server';
+// process.env.DEBUG = process.env.DEBUG || '*INFO* *WARN* *ERROR*';
 
-const port = 5000;
+const config = require('./config');
+// import config from './config.js'
+
+/* eslint-disable no-console */
+// console.log('process.env.DEBUG:', process.env.DEBUG);
+// console.log('config.js:\n%s', JSON.stringify(config, null, '  '));
+/* eslint-enable no-console */
+
+const fs = require('fs');
+const https = require('https');
+const url = require('url');
+const protoo = require('protoo-server');
+const mediasoup = require('mediasoup');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { AwaitQueue } = require('awaitqueue');
+const Logger = require('./lib/Logger');
+const Room = require('./lib/Room');
+const interactiveServer = require('./lib/interactiveServer');
+const interactiveClient = require('./lib/interactiveClient');
+
+const logger = new Logger();
+
+// Async queue to manage rooms.
+// @type {AwaitQueue}
+const queue = new AwaitQueue();
+
+// Map of Room instances indexed by roomId.
+// @type {Map<Number, Room>}
+const rooms = new Map();
+
+// HTTPS server.
+// @type {https.Server}
+let httpsServer;
+
+// Express application.
+// @type {Function}
+let expressApp;
+
+// Protoo WebSocket server.
+// @type {protoo.WebSocketServer}
+let protooWebSocketServer;
+
+// mediasoup Workers.
+// @type {Array<mediasoup.Worker>}
+const mediasoupWorkers = [];
+
+// Index of next mediasoup Worker to use.
+// @type {Number}
+let nextMediasoupWorkerIdx = 0;
