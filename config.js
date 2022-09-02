2c493054a4ea7ce634db8578374314c1a6fc2bf7
Idriss <hi_hamzaoui@esi.dz>
2022-09-02 16:10:17 +0100

    Video recording is working using FFmpeg

--- config.js
+++ config.js
@@ -144,14 +144,14 @@ module.exports =
         },
 
         webRtcTransport: {
-            listenIps: [{ ip: '0.0.0.0', announcedIp: '192.168.0.100' }], // TODO: Change announcedIp to your external IP or domain name
+            listenIps: [{ ip: '0.0.0.0', announcedIp: '192.168.1.11' }], // TODO: Change announcedIp to your external IP or domain name
             enableUdp: true,
             enableTcp: true,
             preferUdp: true,
             maxIncomingBitrate: 1500000
         },
         plainRtpTransport: {
-            listenIp: { ip: '0.0.0.0', announcedIp: '192.168.0.100' }, // TODO: Change announcedIp to your external IP or domain name
+            listenIp: { ip: '0.0.0.0', announcedIp: '192.168.1.11' }, // TODO: Change announcedIp to your external IP or domain name
             rtcpMux: true,
             comedia: false
         }
